3b6e558d8cc0e0fda1c9dda2ba613a5ead3ac376
Nikhil Agrawal <nikhilagrawal2002@gmail.com>
2024-09-27 17:27:20 +0530

    Added Radio Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 const App = () => {
   const [username, setusername] = useState("")
   const [error, seterror] = useState(false)
+  const [gender, setgender] = useState("male")
 
   const SubmitHandler = (e) => {
     e.preventDefault()
-    console.log(username)
+    console.log(username,gender)
   }
   return (
     <div>
@@ -16,7 +17,10 @@ const App = () => {
         value={username} 
         onInput={(e)=> e.target.value.length<5 ? seterror(true):seterror(false)}/>
 
-        <button type="submit" disabled={error}>Submit</button>
+        <button type="submit" disabled={error}>Submit</button> <br /><br />
+
+        <input type="radio" defaultValue="male" name="gender" checked={gender==="male"?true:false} onChange={(e)=> setgender(e.target.value)}/> Male <br /><br />
+        <input type="radio" defaultValue="female" name="gender" checked={gender==="female"?true:false} onChange={(e)=> setgender(e.target.value)}/> Female 
 
         {error && <p style={{color:"red"}}>Username should be at least 5 characters long</p>} 
       </form>
