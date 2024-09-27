c8a834d6c9c09a403d56ad66b28f10f0b22f9b94
Nikhil Agrawal <nikhilagrawal2002@gmail.com>
2024-09-27 17:13:39 +0530

    Form with validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 const App = () => {
+  const [username, setusername] = useState("")
+  const [error, seterror] = useState(false)
+
+  const SubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(username)
+  }
   return (
     <div>
-      <h1>Hello from React</h1>
-      <h1 className="text-7xl font-bold underline">Hello world!</h1>
+      <form onSubmit={SubmitHandler}>
+        <input type="text" name="username" placeholder="username" 
+        onChange={(e)=>{setusername(e.target.value)}} 
+        value={username} 
+        onInput={(e)=> e.target.value.length<5 ? seterror(true):seterror(false)}/>
+
+        <button type="submit" disabled={error}>Submit</button>
+
+        {error && <p style={{color:"red"}}>Username should be at least 5 characters long</p>} 
+      </form>
     </div>
   );
 };
